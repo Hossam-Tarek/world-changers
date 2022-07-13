15c4e22e6a544c8549037c8449042014d13ed796
khaleedahmmed <khaleed.ahmmed@gmail.com>
2022-07-14 01:13:11 +0200

    teacher register

--- app/Http/Controllers/Teacher/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Teacher/Auth/RegisteredUserController.php
@@ -61,8 +61,11 @@ class RegisteredUserController extends Controller
             'school' => 'required|string|max:255',
             'brief' => 'nullable|string',
             'youtube' => 'nullable|string',
+            'images.*' => 'nullable|mimes:jpeg,jpg,png,gif|max:10000',
+            'avatar' => 'nullable|mimes:jpeg,jpg,png,gif|max:10000'
         ]);
         $data['password'] = Hash::make($request->password);
+        $data['image'] = self::uploadFile($request->file('avatar'), 'teachers/');
         $teacher = Teacher::create($data);
         foreach ($request->phones as $value) {
             $phone = new Phone();
