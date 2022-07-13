f55ebbac24246ce49259085b0040b9fdb5b26b2c
Hossam Tarek <hossamtarek250@gmail.com>
2022-07-14 01:54:03 +0200

    Add teacher layout

--- resources/views/components/teacher/includes/sidebar.blade.php
+++ resources/views/components/teacher/includes/sidebar.blade.php
@@ -1,60 +1,40 @@
-<!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="{{ route('teacher.home') }}" class="brand-link">
-        <img src="{{ asset(getSetting('logo')) }}" alt="Logo" class="brand-image elevation-3" style="opacity: .8; border-radius: 5px">
-        <span class="brand-text font-weight-light">{{ env('APP_NAME') }}</span>
-    </a>
-
-    <!-- Sidebar -->
-    <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <img src="{{ asset(auth('teacher')->user()->image ?? 'admin-assets/dist/img/user2-160x160.jpg') }}" style="max-height: 30px; width: auto" class="img-circle elevation-2" alt="User Image">
-            </div>
-            <div class="info">
-                <a href="{{ route('teacher.profile.edit') }}" class="d-block">{{ auth('teacher')->user()->name }}</a>
-            </div>
-        </div>
-
-        <!-- SidebarSearch Form -->
-        <div class="form-inline">
-            <div class="input-group" data-widget="sidebar-search">
-                <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-                <div class="input-group-append">
-                    <button class="btn btn-sidebar">
-                        <i class="fas fa-search fa-fw"></i>
-                    </button>
-                </div>
-            </div>
-        </div>
-
-        <!-- Sidebar Menu -->
-        <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                <!-- Add icons to the links using the .nav-icon class
-                     with font-awesome or any other icon font library -->
-                <li class="nav-item menu-open">
-                    <a href="{{ route('teacher.home') }}" class="nav-link active">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
-                        <p>{{ __('teacher.dashboard') }}</p>
-                    </a>
-                </li>
-
-                <li class="nav-item">
-                    <form method="POST" id="logout-form" action="{{ route('teacher.logout') }}">
-                        @csrf
-                    </form>
-                    <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
-                       class="nav-link">
-                        <i class="nav-icon far fa-circle text-danger"></i>
-                        <p class="text">{{ __('teacher.logout') }}</p>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-        <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
-</aside>
+<div class="open-nav-on-mobile">
+    <i class="fas fa-solid fa-bars" style="color: #333"></i>
+</div>
+<div class="navigation" >
+    <ul>
+        <li class="item">
+            <a href="">
+                <i class="fas fa-home icon"></i>
+                <span class="title">صفحتي الرئيسية</span>
+            </a>
+        </li>
+        <li class="item active">
+            <a href="">
+                <i class="far fa-edit icon"></i>
+                <span class="title">إنشاء امتحان</span>
+            </a>
+        </li>
+        <li class="item ">
+            <a href="">
+                <i class="fas fa-user fa-solid icon"></i>
+                <span class="title">الطلاب الذين يتابعونني</span>
+            </a>
+        </li>
+        <li class="item">
+            <a href="">
+                <i class="fas fa-cog icon"></i>
+                <span class="title">الإعدادات</span>
+            </a>
+        </li>
+        <li class="item">
+            <form method="POST" id="logout-form" action="{{ route('teacher.logout') }}">
+                @csrf
+            </form>
+            <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                <i class="fas fa-sign-out-alt icon"></i>
+                <span class="title">الخروج</span>
+            </a>
+        </li>
+    </ul>
+</div>
