5e7e327115da3367b24ce1002b16c2ea9dd5d846
Hossam Tarek <hossamtarek250@gmail.com>
2022-07-16 20:18:06 +0200

    Add change password page (#22)

---
+++ resources/views/teacher/change-password.blade.php
@@ -0,0 +1,35 @@
+<x-teacher.layouts.master>
+    <section class="sec register">
+        <div class="container">
+            <header class="main-header">
+                <span></span>
+                <h2>تغيير الرقم السري</h2>
+            </header>
+
+            <form action="{{ route('teacher.password.change') }}" method="POST" class="needs-validation" novalidate>
+                @csrf
+
+                <div class="row d-flex justify-content-center">
+                    <div class="col-md-6 mx-0 px-0">
+
+                        <x-form.input type="password" name="password" class="col-md-12 mb-2" place-holder="كلمة المرور" required>
+                            كلمة المرور<span class="red">*</span>
+                        </x-form.input>
+
+                        <x-form.input type="password" name="new_password" class="col-md-12 mb-2" place-holder="كلمة المرور الجديدة" required>
+                            كلمة المرور الجديدة<span class="red">*</span>
+                        </x-form.input>
+
+                        <x-form.input type="password" name="new_password_confirmation" class="col-md-12 mb-2" place-holder="تأكيد كلمة المرور" required>
+                            تأكيد كلمة المرور<span class="red">*</span>
+                        </x-form.input>
+                        <button type="submit" class="btn w-full">
+                            تغيير الرقم السري
+                            <img src="{{ asset('assets/images/home/arrow-left.svg') }}" alt="" />
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </section>
+</x-teacher.layouts.master>
